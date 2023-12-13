29e9c13d75d8e506741bc10162620bd902a55ac3
EvanSarwer <evansarwer1@gmail.com>
2023-12-14 03:19:45 +0600

    in common controller Vaccine Registration details PDF page

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -3,10 +3,80 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\VaccineTake;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use stdClass;
 
 class CommonController extends Controller
 {
-    //
+    public function VaccinationDetailsPdfView($id)
+    {$vaccine_take = VaccineTake::findOrFail($id);
+        $vaccine_take->user = $vaccine_take->user;
+        $vaccine_take->vaccine = $vaccine_take->vaccine;
+        $vaccine_take->center = $vaccine_take->center;
+
+        $vaccine_doses = array();
+        $dose_info = new stdClass();
+        $dose_info->dose_number = 1;
+        $dose_info->dose_date = Carbon::parse($vaccine_take->first_dose_date)->format('Y-m-d');
+        array_push($vaccine_doses,$dose_info);
+        
+        if($vaccine_take->completed_doses >= $vaccine_take->vaccine->doses_required){
+            $vaccine_take->vaccine_status = 'Completed';
+        }else{
+            $vaccine_take->vaccine_status = 'Pending';
+        }
+
+        if($vaccine_take->vaccine->doses_required > 1){
+            $dose_gap_number = $vaccine_take->vaccine->dose_gap_number;
+            $dose_gap_time = $vaccine_take->vaccine->dose_gap_time;
+            $first_dose_date = $vaccine_take->first_dose_date;
+            
+
+            for($i = 2; $i <= $vaccine_take->vaccine->doses_required; $i++){
+
+                if($dose_gap_number !=null && $dose_gap_time !=null){
+
+                    $dose_date = '';
+                    if($dose_gap_time == 'day'){
+                        $dose_date = Carbon::parse($first_dose_date)->addDays(($i - 1) * $dose_gap_number)->format('Y-m-d');
+                    }else if($dose_gap_time == 'week'){
+                        $dose_date = Carbon::parse($first_dose_date)->addWeeks(($i - 1) * $dose_gap_number)->format('Y-m-d');
+                    }else if($dose_gap_time == 'month'){
+                        $dose_date = Carbon::parse($first_dose_date)->addMonths(($i - 1) * $dose_gap_number)->format('Y-m-d');
+                    }else if($dose_gap_time == 'year'){
+                        $dose_date = Carbon::parse($first_dose_date)->addYears(($i - 1) * $dose_gap_number)->format('Y-m-d');
+                    }
+
+                    $dose_info = new stdClass();   
+                    $dose_info->dose_number = $i;
+                    $dose_info->dose_date = $dose_date;
+                    
+                    array_push($vaccine_doses,$dose_info);
+                    // $vaccine_take->vaccine_doses[] = [
+                    //     'dose_number' => $i,
+                    //     'dose_date' => $dose_date,
+                    // ];
+
+                }else{
+
+                    $dose_info = new stdClass();   
+                    $dose_info->dose_number = $i;
+                    $dose_info->dose_date = 'Not Set Yet';
+                    
+                    array_push($vaccine_doses,$dose_info);
+                }  
+
+            }
+
+            
+
+            
+        }
+
+        return view('common.vaccine_registrationDetails_pdf',compact('vaccine_take','vaccine_doses'));
+    
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/admin/vaccination-details/pdf/{id}', [CommonController::class, 'VaccinationDetailsPdfView'])->name('admin.vaccination.pdf.details');
+
 });
 
 
